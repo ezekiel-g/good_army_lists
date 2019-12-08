25b3a1193dc89208831d9c9a43394e1e41495a10
Ezekiel Giordano <etgfiles@Ezekiels-MacBook-Air.local>
2019-12-08 04:52:54 -0500

    donations 2

--- app/javascript/react/kow/containers/UnitEntriesFormContainer.js
+++ app/javascript/react/kow/containers/UnitEntriesFormContainer.js
@@ -4333,24 +4333,17 @@ class UnitEntriesFormContainer extends Component {
 				</div>
 				<div className="email-div">
 					<span className="user-select-none">Email:{' '}</span>admin@goodarmylists.com
-					<form className="user-select-none" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_donations" />
-					<input type="hidden" name="business" value="admin@goodarmylists.com" />
-					<input type="hidden" name="currency_code" value="USD" />
-					<input
-						type="image"
-						src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-						border="0"
-						name="submit"
-						title="PayPal - The safer, easier way to pay online!"
-						alt="Donate with PayPal button"
-					/>
-					<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" className="paypal-image" />
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_donations" />
+						<input type="hidden" name="business" value="admin@goodarmylists.com" />
+						<input type="hidden" name="currency_code" value="USD" />
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+					<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 					</form>
 				</div>
 			</div>
-
 		return (
+						
 			<div>
 				<div className="army-dropdown-section">
 					<div className="main-page-link">
